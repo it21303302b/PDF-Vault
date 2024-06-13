a095589dff7a479d5c0afa472e22b8cfc6e0b153
bimsara weerakoon <99312076+it21303302b@users.noreply.github.com>
2024-06-13 14:10:45 +0530

    logout

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,21 +1,31 @@
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 const Navbar = () => {
-    return(
-        <header>
-            <div className="container">
-                <Link to="/">
-                    <h1>PDF Vault</h1>
-                </Link>
-                <nav>
-                    <div>
-                        <Link to="/login">Login</Link>
-                        <Link to="/signup">Signup</Link>
-                    </div>
-                </nav>
-            </div>
-        </header>
-    )
+  const { logout } = useLogout()
+
+  const handleClick = () => {
+    logout()
+  }
+
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>PDF Vault</h1>
+        </Link>
+        <nav>
+          <div>
+            <button onClick={handleClick}>Log out</button>
+          </div>
+          <div>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+          </div>
+        </nav>
+      </div>
+    </header>
+  )
 }
 
 export default Navbar
