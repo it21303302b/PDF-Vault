ba55a78d1e028af2820e7bb2acad67756c26b5fc
bimsara weerakoon <99312076+it21303302b@users.noreply.github.com>
2024-06-13 13:45:12 +0530

    login frontend basic

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { PdfsContextProvider } from './context/PDFContext';
+import { AuthContextProvider } from './context/AuthContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <PdfsContextProvider>
-    <App />
-    </PdfsContextProvider>
+    <AuthContextProvider>
+      <PdfsContextProvider>
+        <App />
+      </PdfsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
